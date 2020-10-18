190b477008adcbd3c56c28120986c18ed488d04b
Danyal Farahany <farahany.danyal@gmail.com>
2020-10-18 11:02:39 +0330

    im gonna cry

--- Compiler/compiler.py
+++ Compiler/compiler.py
@@ -4,4 +4,7 @@ def scan():
     exec(open(os.path.realpath('./Compiler/scanner.py')).read())
 
 # scan()
-exec(open(os.path.realpath('./Compiler/scanner.py')).read()) #wtaf
+# exec(open(os.path.realpath('./Compiler/scanner.py')).read()) #wtaf
+sth = os.path.dirname("compiler.py")
+sth = sth.replace("compiler","scanner")
+print(sth)
